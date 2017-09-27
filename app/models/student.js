971ca38ff2f993f46891be7ca7a379ff5ed54368
delpast1 <lamquanguit@gmail.com>
2017-09-27 14:44:19 +0700

    this is quiz app

--- app/models/student.js
+++ app/models/student.js
@@ -1,11 +1,10 @@
 var mongoose = require('../config/index').db;
 var Schema = mongoose.Schema;
-var info = require('mUser');
-var test = require('mTest');
+var info = require('user');
+var test = require('tests');
 
 module.exports = mongoose.model('Student', new Schema({
     personalInfo: info,
-    role: {type: String, default: 'Student'},
     tests: [{
         testId: String,
         result: [{
--- app/models/test.js
+++ app/models/test.js
@@ -1,6 +1,6 @@
 var mongoose = require('../config/index').db;
 var Schema = mongoose.Schema;
-var quiz = require('mQuiz');
+var quiz = require('quiz');
 
 module.exports = mongoose.model('Test', new Schema({
     name: "String",
